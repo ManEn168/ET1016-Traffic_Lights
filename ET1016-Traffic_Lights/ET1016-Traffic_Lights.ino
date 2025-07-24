a682a303efca79e3f31e20eeedfd02a93e3c65c6
ManEn168 <MANEN.25@ichat.sp.edu.sg>
2025-07-24 15:48:19 +0800

    Create ET1016-Traffic_Lights.ino

---
+++ ET1016-Traffic_Lights/ET1016-Traffic_Lights.ino
@@ -0,0 +1,17 @@
+
+//Name:Leccia, Man En
+//Admin No : 2
+//CLASS: DCEP/FT/A1/09
+//
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
