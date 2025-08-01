b95a37dacc2fec7f8ec3fb799c87b6fa56eced95
ManEn168 <MANEN.25@ichat.sp.edu.sg>
2025-08-01 12:53:27 +0800

    Update ET1016-Traffic_Lights.ino

--- ET1016-Traffic_Lights/ET1016-Traffic_Lights.ino
+++ ET1016-Traffic_Lights/ET1016-Traffic_Lights.ino
@@ -1,14 +1,30 @@
 
 //Name:Leccia, Man En
-//Admin No : 2
+//Admin No : 2517254,2516576
 //CLASS: DCEP/FT/A1/09
 //
 
+#include <Wire.h>
+#include "RichShieldPassiveBuzzer.h"
+#define PassiveBuzzerPin 3
+PassiveBuzzer buz(PassiveBuzzerPin);
+
+#define NOTE_M4  698
+#define LED_RED 4
+#define LED_GREEN 5
+#define LED_BLUE 6
+#define LED_YELLOW 7
+#define BUTTONK1 8
+#define BUTTONK2 9
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(BUTTONK1,INPUT_PULLUP);
+  pinMode(BUTTONK2, INPUT_PULLUP);
+  pinMode(LED_RED,OUTPUT);
+  pinMode(LED_BLUE,OUTPUT);
+  pinMode(LED_GREEN,OUTPUT);
+  pinMode(LED_YELLOW,OUTPUT); 
 }
 
 void loop() {
